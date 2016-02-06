5c64922dac91757e76272d586473d4af99298de7
ankihg <hagelin.annika@gmail.com>
2016-02-05 17:23:56 -0800

    textdiv color change

--- scripts/postController.js
+++ scripts/postController.js
@@ -9,6 +9,7 @@
     console.log(post);
     post.toPageHTML();
     makeHomeLink();
+    setClickTextDivColor();
   };
 
   function ui() {
@@ -27,6 +28,39 @@
 
     $('#home-link').append(html);
   }
+
+function setClickTextDivColor() {
+  $('#textDiv').on('click', function() {
+    var background;
+    var color;
+
+    var r = Math.random()*10;
+
+    if (r < 2) {
+    background = "#000000";
+    color = "#FFFFFF";
+    } else if (r < 3) {
+    background = "#ffff00";
+    color = "#023ef2";
+    } else if (r < 4) {
+    background = "#00ff00";
+    color = "#ff0000";
+    } else if (r < 5) {
+    background = "#ff0000";
+    color = "#00ff00";
+    } else if (r < 6) {
+    background = "#023ef2";
+    color = "#ffff00";
+    } else {
+    background = "#FFFFFF";
+    color = "#000000";
+    }
+
+    $(this).css('background-color', background);
+    $(this).css('color', color);
+
+  });
+}
 
 
   module.postController = postController;
